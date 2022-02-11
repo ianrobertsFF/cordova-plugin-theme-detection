059da348c09923c81a879d11a1e7c0e12a599ab5
ianrobertsFF <91917328+ianrobertsFF@users.noreply.github.com>
2022-02-11 18:30:59 +0000

    Promisify Result

--- www/ThemeDetection.js
+++ www/ThemeDetection.js
@@ -2,10 +2,24 @@ var exec = require("cordova/exec");
 
 var CLASS = "ThemeDetection";
 
-exports.isAvailable = function(success, error) {
-  exec(success, error, CLASS, "isAvailable", []);
-};
+exports.isAvailable = function (success, error) {
+    return new Promise((resolve, reject) => {
+        exec((success)=>{
+            resolve(success);
+        },
+        (error)=> {
+            reject(error)
+        }, CLASS, "isAvailable", []);
+    });
+}
 
-exports.isDarkModeEnabled = function(success, error) {
-  exec(success, error, CLASS, "isDarkModeEnabled", []);
-};
+exports.isDarkModeEnabled = function (success, error) {
+    return new Promise((resolve, reject) => {
+        exec((success)=>{
+            resolve(success);
+        },
+        (error)=> {
+            reject(error)
+        }, CLASS, "isDarkModeEnabled", []);
+    });
+}
